500ce054b8e413e02b03a00faf8fcad712fc3add
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2012-01-18 08:51:13 +0000

    Fixes #7399, Refs #7680, #8877, #8953: Allow Href to take care of path joining. Tested in 0.11 and 0.13dev-r10909.

--- tracmenus/web_ui.py
+++ tracmenus/web_ui.py
@@ -136,7 +136,7 @@ class MenuManagerModule(Component):
                 value=self.config[menu_name].getbool(option, True)
             elif prop_name=='href':
                 value = value.replace('$PATH_INFO', req.path_info)
-                menu[name]['label']=menu[name].setdefault('label', html.a())(href=value.startswith('/') and req.href().rstrip('/') and value or value)
+                menu[name]['label']=menu[name].setdefault('label', html.a())(href=req.href(value))
             elif prop_name=='label':
                 menu[name].setdefault('label', html.a(href='#'))(value)
                 continue
